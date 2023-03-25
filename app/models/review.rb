bdcb488dbafcf7408f10d800ba32a3ee750822a0
Mathew Hocking <matt8443478@gmail.com>
2023-03-25 16:15:42 +1100

    completed challenge

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
+  RATINGS = (0..5).to_a
+
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: RATINGS }, numericality: { only_integer: true }
   belongs_to :restaurant
 end
